39fca042dddba337fa31a6c6b384f89aeaa66b07
Rob Harrop <rob@techcraft.co>
2022-07-24 21:29:42 +0100

    Mostly usable config - a great starting point. Still needs tray apps

--- xmonad.hs
+++ xmonad.hs
@@ -1,39 +1,68 @@
-import System.Exit (exitSuccess)
+import qualified Data.Map as M
+import Data.Maybe (fromJust)
 import XMonad
-import XMonad.Hooks.DynamicLog (dynamicLogWithPP, xmobarPP)
-import XMonad.Hooks.EwmhDesktops (ewmh)
-import XMonad.Hooks.ManageDocks (avoidStruts, docks)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 import XMonad.Layout.ThreeColumns
-import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig (additionalKeysP, mkKeymap)
-
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.NoBorders
+import XMonad.Util.ClickableWorkspaces
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Loggers
 
 main :: IO ()
 main =
-  do
-    xmobar <- spawnPipe "xmobar"
-    xmonad $
-      docks $
-        ewmh $
-          def
-            { modMask = mod4Mask,
-              layoutHook = myLayout
-            }
-            `additionalKeysP` myKeys
+  xmonad
+    . ewmhFullscreen
+    . ewmh
+    . withEasySB (statusBarProp "xmobar $HOME/.config/xmonad/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
+    $ myConfig
 
 myBrowser = "firefox"
 
 myEmacs = "emacs"
 
-myKeys =
-  [ ("M-e e", spawn myEmacs),
-    ("M-b", spawn myBrowser)
-  ]
+myWorkspaces = ["dev", "www", "comms", "sys"] ++ map show [5 .. 9]
 
-myLayout = avoidStruts $ Full ||| columns ||| Mirror columns ||| tcm
+myWorkspaceIndices = M.fromList $ zip myWorkspaces [1 ..]
+
+myConfig =
+  def
+    { modMask = mod4Mask,
+      layoutHook = smartBorders myLayout,
+      workspaces = myWorkspaces
+    }
+    `additionalKeysP` [ ("M-S-b", spawn myBrowser),
+                        ("M-e e", spawn myEmacs),
+                        ("M-S-<Enter>", spawn "kitty")
+                      ]
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| tcm
   where
-    columns = Tall 1 (3 / 100) (1 / 2)
-    tcm = ThreeColMid 1 (3 / 100) (1 / 2)
+    tiled = Tall 1 delta ratio
+    tcm = ThreeColMid 1 delta ratio
+    delta = (3 / 100)
+    ratio = (1 / 2)
 
-myLogHook xmobar = dynamicLogWithPP $ xmobarPP
+myXmobarPP :: PP
+myXmobarPP =
+  def
+    { ppCurrent = xmobarColor draculaBackground draculaGreen,
+      ppVisible = xmobarColor draculaBackground draculaGreen . xmobarClickable,
+      ppHidden = xmobarColor draculaForeground draculaComment . xmobarClickable,
+      ppHiddenNoWindows = xmobarColor draculaForeground draculaBackground,
+      ppSep = " | "
+    }
+
+xmobarClickable ws = clickableWrap (i - 1) ws
+  where
+    i = fromJust $ M.lookup ws myWorkspaceIndices
+
+draculaBackground = "#282a36"
+
+draculaForeground = "#f8f8f8"
+
+draculaGreen = "#50fa7b"
+
+draculaComment = "#6272a4"
